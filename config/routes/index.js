257844dd81d63731af16f7da889025d8bd66e8a9
Magnus Andersson <bredsjomagnus@yahoo.se>
2017-11-18 09:03:00 +0000

    Memory module first commit

---
+++ config/routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var navbarjson = require('../navbar.json');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render(
+        'index',
+        {
+            title: 'RV2 | maaa16',
+            navlist: navbarjson,
+            thisurl: req.url
+        });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res) {
+    res.render(
+        'about',
+        {
+            title: 'OM | maaa16',
+            navlist: navbarjson,
+            thisurl: req.url
+        });
+});
+
+/* GET reports page. */
+router.get('/reports', function(req, res) {
+    res.render(
+        'reports',
+        {
+            title: 'RAPPORTER | maaa16',
+            navlist: navbarjson,
+            thisurl: req.url,
+        });
+});
+
+module.exports = router;
